08c7882d02b692010a02725790f894d7a60487b6
Yash <yashgyy@gmail.com>
2024-05-02 14:09:54 +0530

    Made UI

--- javascript.js
+++ javascript.js
@@ -3,41 +3,50 @@ console.log("Hello")
 let humanScore = 0
 let computerScore = 0
 
+let temp1 = document.querySelector(".rock")
+let temp2 = document.querySelector(".scissors")
+let temp3 = document.querySelector(".paper")
+let heading = document.querySelector('h2')
+temp1.addEventListener("click",Handler)
+temp2.addEventListener("click",Handler)
+temp3.addEventListener("click",Handler)
+
+
 function playRound(humanChoice,computerChoice){
     humanChoice = humanChoice.toLowerCase();
     computerChoice  = computerChoice.toLowerCase();
 
     switch(computerChoice){
-        case "rock": if(humanChoice=="rock") {console.log("Draw"); humanScore++; computerScore++;}
+        case "rock": if(humanChoice=="rock") {heading.textContent = ("Draw"); humanScore++; computerScore++;}
                      else if(humanChoice=="scissors"){
                         computerScore++;
-                        console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+                        heading.textContent = (`You lose! ${computerChoice} beats ${humanChoice}`);
                      }
                      else {
                         humanScore++;
-                        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+                        heading.textContent = (`You win! ${humanChoice} beats ${computerChoice}`);
                      }
                      break;
 
-        case "paper": if(humanChoice=="paper") {console.log("Draw"); humanScore++; computerScore++;}
+        case "paper": if(humanChoice=="paper") {heading.textContent = ("Draw"); humanScore++; computerScore++;}
                      else if(humanChoice=="rock"){
                         computerScore++;
-                        console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+                        heading.textContent = (`You lose! ${computerChoice} beats ${humanChoice}`);
                      }
                      else {
                         humanScore++;
-                        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+                        heading.textContent = (`You win! ${humanChoice} beats ${computerChoice}`);
                      }
                      break;
 
-        case "scissors": if(humanChoice=="scissors") {console.log("Draw"); humanScore++; computerScore++;}
+        case "scissors": if(humanChoice=="scissors") {heading.textContent = ("Draw"); humanScore++; computerScore++;}
                      else if(humanChoice=="paper"){
                         computerScore++;
-                        console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+                        heading.textContent = (`You lose! ${computerChoice} beats ${humanChoice}`);
                      }
                      else {
                         humanScore++;
-                        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+                        heading.textContent = (`You win! ${humanChoice} beats ${computerChoice}`);
                      }
                      break;
     }
@@ -58,21 +67,37 @@ function getHumanChoice() {
     return prompt("rock,paper or scissors");
 }
 
-function playGame() {
+// function playGame() {
 
-for(let i=0;i<5;i++){
-    const humanSelection = getHumanChoice();
-    const computerSelection = getComputerChoice();
-    playRound(humanSelection,computerSelection)
+// for(let i=0;i<5;i++){
+//     const humanSelection = getHumanChoice();
+//     const computerSelection = getComputerChoice();
+//     playRound(humanSelection,computerSelection)
+// }
+
+// if(humanScore>computerScore) return `Final Score ${humanScore}-${computerScore}. You won`;
+// else if(computerScore>humanScore) return `Final Score ${humanScore}-${computerScore}. You lost`;
+// else return `Final Score ${humanScore}-${computerScore}. Draw`;
+
+// }
+
+// console.log(playGame())
+
+
+function Handler(button){
+  // console.log(button.currentTarget.textContent)
+
+   let humanSelection = button.currentTarget.textContent;
+   let computerSelection = getComputerChoice();
+   playRound(humanSelection,computerSelection)
+   let score = document.querySelector(".score h2");
+   score.textContent = `Score Human-${humanScore} vs Computer-${computerScore}`
+
+
+
 }
 
-if(humanScore>computerScore) return `Final Score ${humanScore}-${computerScore}. You won`;
-else if(computerScore>humanScore) return `Final Score ${humanScore}-${computerScore}. You lost`;
-else return `Final Score ${humanScore}-${computerScore}. Draw`;
 
-}
-
-console.log(playGame())
 
 
 
